73b8aa6adeb32ef723e43522fadd5ff9799de1be
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-24 10:07:14 +0700

    add response when user is logged in

--- user/index.js
+++ user/index.js
@@ -4,6 +4,12 @@ var auth = require('./auth')
 var router = express.Router();
 
 router.post('/login', auth.login)
-router.get('/check_auth', auth.auth)
+router.get('/check_auth', auth.auth, (req, res, next) => {
+    res.json({
+        status: 'success',
+        success: true,
+        login: true
+    })
+})
 
 module.exports = router;
