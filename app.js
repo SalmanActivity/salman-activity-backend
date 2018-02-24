947daeb070cb2f96cc09ca1efba46f91e7437f5d
Jauhar Arifin <jauhararifin10@gmail.com>
2018-02-24 10:05:44 +0700

    preparing mongo db

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var config = require('./config')
+var debug = require('debug')('salman-activity-backend:server')
 
-var status = require('./status/index');
+var app = express()
 
-var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/api/v1/status', status);
+app.use('/api/v1/status', require('./status/index'))
+app.use('/api/v1/user', require('./user/index'))
 
-module.exports = app;
+mongoose.connect(config.mongo_connection).then(resp => {
+    debug('connected to mongo server')
+}).catch(error => {
+    debug('error connecting to mongo server')
+})
+
+module.exports = app
---
+++ config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'mongo_connection': '[MONGO_CONNECTION_URI]',
+    'secret_key': '[APPLICATION_SECRET_KEY]'
+}
