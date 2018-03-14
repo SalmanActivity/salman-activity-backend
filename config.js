3a475127e128b54f849c223e8db2fc5654586b48
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-14 18:49:36 +0700

    update config loader, add capability yo load config from environment variables

--- config.js
+++ config.js
@@ -3,4 +3,15 @@ var fs = require('fs')
 var environment = process.env.ENV || 'dev'
 var configurationFile = 'config.' + environment + '.json'
 
-module.exports = JSON.parse(fs.readFileSync(configurationFile, 'utf8'))
+var config
+
+if (fs.existsSync(configurationFile))
+    config = JSON.parse(fs.readFileSync(configurationFile, 'utf8'))
+else {
+    config = {}
+    for (let key in process.env)
+        if (key.startsWith('CONFIG_'))
+            config[key.substr('CONFIG_'.length)] = process.env[key]
+}
+
+module.exports = config
