8437e7f7d8132cddbd0b94f09bb855d142d6acf6
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-14 14:33:34 +0700

    add seeder for development database

---
+++ seed.js
@@ -0,0 +1,30 @@
+var userModel = require('./user/user')
+var divisionModel = require('./division/division')
+var passwordHash = require('password-hash')
+
+let division1 = new divisionModel({
+    name: 'division_1',
+    enabled: true
+})
+
+let user1 = new userModel({
+    name: 'Test User 1',
+    username: 'test_user_1',
+    password: passwordHash.generate('test_user_1_pass'),
+    division: division1,
+    enabled: true,
+    admin: false
+})
+
+let admin1 = new userModel({
+    name: 'Test Admin 1',
+    username: 'test_admin_1',
+    password: passwordHash.generate('test_admin_1_pass'),
+    division: null,
+    enabled: true,
+    admin: true
+})
+
+module.exports = [
+    division1, user1, admin1
+]
