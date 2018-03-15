8031a6a769ee8cbf6a4d06f95f04d463f36cfe13
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-15 15:48:38 +0700

    standarize crud get functions

--- crud/get.js
+++ crud/get.js
@@ -1,85 +1,67 @@
 var bluebird = require('bluebird')
 var async = require('async')
 var error = require('./error')
+var crudUtil = require('./util')
 
-function getFetchConvertFilter(fetchFunction, convertFunction, filterFunction, filterField) {
+function readMany(option) {
+    if (option.filterOne && !option.filterMany)
+        option.filterMany = crudUtil.oneToManyFunction(option.filterOne)
+
+    if (option.convertOne && !option.convertMany)
+        option.convertMany = crudUtil.oneToManyFunction(option.convertOne)
+
+    if (option.filterFieldOne && !option.filterFieldMany)
+        option.filterFieldMany = crudUtil.oneToManyFunction(option.filterFieldOne)
+
+    if (!option.init)
+        option.init = (req, context, callback) => callback(null,null)
+    
     return (req, res, next) => {
-        // fetch
-        bluebird.promisify(fetchFunction)(req)
-        .then(valArr => {
-            // convert
-            let parallelArr = valArr.map(val => convertFunction.bind(this, req, val))
-            return bluebird.promisify(async.parallel)(parallelArr)
-        })
-        .then(valArr => {
-            // filter
-            let parallelArr = valArr.map(val => filterFunction.bind(this, req, val))
-            return bluebird.promisify(async.parallel)(parallelArr)
-            .then(filteredArr => {
-                let resultArr = []
-                for (let i = 0; i < valArr.length; i++)
-                    if (filteredArr[i])
-                        resultArr.push(valArr[i])
-                return resultArr
-            })
-        })
-        .then(valArr => {
-            // filter field
-            let parallelArr = valArr.map(val => filterField.bind(this, req, val))
-            return bluebird.promisify(async.parallel)(parallelArr)
-        })
-        .then(valArr => {
-            // display result
-            res.status(200).json(valArr)
-        })
+        let context = {}
+        return bluebird.promisify(option.fetchMany)(req, context)
+        .then(valArr => bluebird.promisify(option.convertMany)(valArr, context))
+        .then(valArr => bluebird.promisify(option.filterFieldMany)(valArr, context))
+        .then(valArr => res.status(200).json(valArr))
         .catch(error.displayError(res, 'cannot fetch specific object'))
     }
 }
 
-function getOneFetchConvertFilter(fetchFunction, convertFunction, filterFunction, filterField) {
+function readOne(option) {
+    if (!option.convertOne && option.convertMany)
+        option.convertOne = crudUtil.manyToOneFunction(option.convertMany)
+
+    if (!option.filterOne && option.filterMany)
+        option.filterOne = crudUtil.manyToOneFunction(option.filterMany)
+
+    if (!option.filterFieldOne && option.filterFieldMany)
+        option.filterFieldOne = crudUtil.manyToOneFunction(option.filterFieldMany)
+    
+    if (!option.init)
+        option.init = (req, context, callback) => callback(null,null)
+
     return (req, res, next) => {
-        // fetch
-        bluebird.promisify(fetchFunction)(req)
+        let context = {}
+        return bluebird.promisify(option.init)(req, context)
+        .then(res => bluebird.promisify(option.fetchOne)(req, context))
         .then(result => {
-            // convert
             if (result)
-                return bluebird.promisify(convertFunction)(req, result)
+                return bluebird.promisify(option.convertOne)(result, context)
             return Promise.reject({status:404, cause: 'object not found'})
         })
         .then(result => {
-            // filter
             if (result)
-                return bluebird.promisify(filterFunction)(req, result)
+                return bluebird.promisify(option.filterOne)(result, context)
                 .then(filtered => filtered ? filtered : Promise.reject({status:404, cause: 'object not found'}))
             return Promise.reject({status:404, cause: 'object not found'})
         })
         .then(result => {
-            // filter field
-            return bluebird.promisify(filterField)(req, result)
+            if (result)
+                return bluebird.promisify(option.filterFieldOne)(result, context)
+            return Promise.reject({status:404, cause: 'object not found'})
         })
-        .then(result => {
-            // display result
-            res.status(200).json(result)
-        })
+        .then(result => res.status(200).json(result))
         .catch(error.displayError(res, 'cannot fetch specific object'))
     }
 }
 
-function fields(fieldArr) {
-    return (req, obj, callback) => {
-        obj = JSON.parse(JSON.stringify(obj))
-
-        let deletedKeys = []
-        if (obj && typeof obj === 'object')
-            for (let key in obj)
-                if (!fieldArr.includes(key))
-                    deletedKeys.push(key)
-
-        for (let key of deletedKeys)
-            delete obj[key]
-
-        callback(null, obj)
-    }
-}
-
-module.exports = { getFetchConvertFilter, getOneFetchConvertFilter, fields }
+module.exports = { readMany, readOne }
--- crud/index.js
+++ crud/index.js
@@ -1,11 +1,16 @@
 let getUtil = require('./get')
 let deleteUtil = require('./delete')
 let postUtil = require('./post')
+let crudUtil = require('./util')
 
 module.exports = {
-    getFetchConvertFilter: getUtil.getFetchConvertFilter,
-    getOneFetchConvertFilter: getUtil.getOneFetchConvertFilter,
     fields: getUtil.fields,
     deleteFindDelete: deleteUtil.deleteFindDelete,
-    postValidateFindInsertConvert: postUtil.postValidateFindInsertConvert
+    postValidateFindInsertConvert: postUtil.postValidateFindInsertConvert,
+
+    readOne: getUtil.readOne,
+    readMany: getUtil.readMany,
+    filterOne: {
+        fields: crudUtil.simpleFilterFieldOne
+    }
 }
