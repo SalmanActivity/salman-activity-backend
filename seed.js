e23658c4d3e0cd0caa89481dbf1220e05cc45251
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-15 21:58:46 +0700

    add seeder for location objects

--- seed.js
+++ seed.js
@@ -1,10 +1,17 @@
 var userModel = require('./user/user')
 var divisionModel = require('./division/division')
+var locationModel = require('./location/location')
 var passwordHash = require('password-hash')
 
 let division1 = new divisionModel({
     _id: '5aa9359a2b21732a73d54068',
-    name: 'division_1',
+    name: 'Divisi 1',
+    enabled: true
+})
+
+let division2 = new divisionModel({
+    _id: '5aaa860001e1901b03651171',
+    name: 'Divisi 2',
     enabled: true
 })
 
@@ -30,6 +37,18 @@ let admin1 = new userModel({
     admin: true
 })
 
+let location1 = new locationModel({
+    _id: '5aaa89e2a892471e3cdc84d8',
+    name: 'Location 1',
+    enabled: true
+})
+
+let location2 = new locationModel({
+    _id: '5aaa89e2a892471e3cdc84d9',
+    name: 'Location 2',
+    enabled: true
+})
+
 module.exports = [
-    division1, user1, admin1
+    division1, division2, user1, admin1, location1, location2
 ]
