6abafb0b322b65ded611f23b57d55133d2fb49d4
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-15 19:54:31 +0700

    add crud logic in division module

--- division/index.js
+++ division/index.js
@@ -1,12 +1,13 @@
 var express = require('express')
+var auth = require('../user/auth')
 var crud = require('./crud')
 
 var router = express.Router();
 
-router.get('/', auth.auth)
-router.get('/:userId', auth.auth)
-router.post('/', auth.auth, auth.admin)
-router.put('/:userId', auth.auth, auth.admin)
-router.delete('/:userId', auth.auth, auth.admin)
+router.get('/', auth.auth, crud.findAllDivisions)
+router.get('/:divisionId', auth.auth, crud.findOneDivision)
+router.post('/', auth.auth, auth.admin, crud.createOneDivision)
+router.put('/:divisionId', auth.auth, auth.admin, crud.updateOneDivision)
+router.delete('/:divisionId', auth.auth, auth.admin, crud.deleteOneDivision)
 
 module.exports = router;
