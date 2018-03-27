e681e6168f4272e59b09575f8408c3e1375b7ef6
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-27 19:36:36 +0700

    enable get user by id

--- user/index.js
+++ user/index.js
@@ -14,7 +14,7 @@ router.post('/check_auth', auth.auth, (req, res, next) => {
 
 router.get('/', auth.auth, crud.findAllUsers)
 router.get('/me', auth.auth, crud.findCurrentUser)
-// router.get('/:userId', auth.auth, crud.findOneUser)
+router.get('/:userId', auth.auth, crud.findOneUser)
 router.post('/', auth.auth, auth.admin, crud.createOneUser)
 router.put('/:userId', auth.auth, auth.admin, crud.updateOneUser)
 router.delete('/:userId', auth.auth, auth.admin, crud.deleteOneUser)
