f8979cfb6cac8b4cb4fa561dce4c70b0b43517c2
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-29 21:56:09 +0700

    fix month index in request fetcher

--- request/crud.js
+++ request/crud.js
@@ -2,13 +2,13 @@ var requestModel = require('./request')
 var crudUtil = require('../crud/index')
 
 let filterRequestMonthYear = (req, callback) => {
-  let monthFilter = new Date().getMonth() + 12
+  let monthFilter = new Date().getMonth()
   let yearFilter = new Date().getFullYear()
 
   if (req.params.month)
-    monthFilter = req.params.month
+    monthFilter = Number(req.params.month) - 1
   if (req.params.year)
-    yearFilter = req.params.year
+    yearFilter = Number(req.params.year) - 1
 
   let startFilter = new Date(yearFilter, monthFilter, 1)
   let endFilter = new Date(yearFilter, monthFilter + 1, 1)
@@ -26,7 +26,7 @@ let filterRequestMonthYear = (req, callback) => {
 let filterRequestByUser = (currentUser, request, callback) => {
   if (request && request.status === 'accepted')
     return callback(null, request)
-  if (currentUser && currentUser.isAdmin)
+  if (currentUser && currentUser.admin)
     return callback(null, request)
   if (currentUser && currentUser.division && currentUser.division.id == request.division.id)
     return callback(null, request)
@@ -36,7 +36,7 @@ let filterRequestByUser = (currentUser, request, callback) => {
 let findRequestInMonth = crudUtil.readMany({
   init: (req, context, callback) => {
     context.user = req.user
-    return req
+    return callback(null, req)
   },
   fetchMany: (req, context, callback) => filterRequestMonthYear(req,callback).populate('issuer', 'division', 'location'),
   convertOne: (obj, context, callback) => callback(null, obj.toJSON ? obj.toJSON() : obj),
