394aa0a3e1c738b0d22f27b70b45c848ff495e74
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-29 20:44:13 +0700

    add request model

---
+++ request/request.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose')
+var userModel = require('../user/user')
+var divisionModel = require('../division/division')
+var locationModel = require('../location/location')
+
+var requestSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    maxlength: 1024,
+  },
+  issuer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: userModel.modelName,
+    required: true
+  },
+  division: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: divisionModel.modelName,
+    required: true
+  },
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: locationModel.modelName,
+    required: true
+  },
+  startTime: {
+    type: Date,
+    required: true
+  },
+  endTime: {
+    type: Date,
+    required: true
+  },
+  participantNumber: Number,
+  participantDescription: {
+    type: String,
+    maxlength: 1024
+  },
+  speaker: {
+    type: String,
+    maxlength: 512
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected']
+  },
+  enabled: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+})
+requestSchema.plugin(require('meanie-mongoose-to-json'))
+
+module.exports = mongoose.model('request', requestSchema)
