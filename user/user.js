eea0123e35bf44d6124ace4ab5b71973a5e1efff
Jauhar Arifin <jauhararifin10@gmail.com>
2018-03-29 20:22:02 +0700

    using 2 spaces indentation

--- user/user.js
+++ user/user.js
@@ -3,37 +3,37 @@ var divisionModel = require('../division/division')
 var divisionSchema = divisionModel.schema
 
 var userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    division: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: divisionModel.modelName
-    },
-    enabled: {
-        type: Boolean,
-        required: true,
-        default: true
-    },
-    admin: {
-        type: Boolean,
-        default: false
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  division: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: divisionModel.modelName
+  },
+  enabled: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  }
 })
 userSchema.plugin(require('meanie-mongoose-to-json'))
 
