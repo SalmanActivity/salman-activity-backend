57a2733a10840c1dc2678701fa4e131134b6a4d2
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-03 22:17:51 +0700

    add accessor interface

---
+++ accessor/accessor.ts
@@ -0,0 +1,15 @@
+import Item from './item'
+
+export interface Accessor<T extends Item> {
+
+  getAll(): Promise<T[]>
+
+  getById(id:string): Promise<T>
+
+  insert(object: T): Promise<T>
+
+  delete(object: T): Promise<T>
+
+  update(object: T): Promise<T>
+
+}
