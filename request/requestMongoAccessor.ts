e997284712965d83dcb3d3c2d639b9a72f5181d1
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-06 06:01:05 +0700

    add get request by month in request accessor

--- request/requestMongoAccessor.ts
+++ request/requestMongoAccessor.ts
@@ -5,6 +5,21 @@ import RequestMongoModel from './requestMongoModel'
 import Request from './request'
 
 export default class RequestMongoAccessor extends MongoAccessor<Request> implements RequestAccessor {
+  async getAllBetween(start:Date, end:Date):Promise<Request[]> {
+
+    let condition = {
+      'startTime': {
+        '$gte': start,
+        '$lt': end
+      }
+    }
+
+    let result:Document[] = await this.mongoModel.find(condition).exec()
+    let promiseList:Promise<Request>[] = result.map<Promise<Request>>((val, idx) => this.docSerializer.serialize(val))
+    let itemArray:Request[] = await Promise.all(promiseList)
+    return itemArray
+  }
+  
   constructor() {
     super(RequestMongoModel)
   }
