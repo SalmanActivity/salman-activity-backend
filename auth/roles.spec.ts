3176758caa9c03f3259d6733e624df991ecc2828
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-05 11:06:40 +0700

    fix auth specification, using function to return endpoint instead of using variable

--- auth/roles.spec.ts
+++ auth/roles.spec.ts
@@ -4,28 +4,30 @@ import { admin } from '.'
 
 describe('admin middleware test', function() {
 
-  var responseSpy
+  var responseSpy, adminEndpoint
   beforeEach(function() {
     responseSpy = {
       status: sinon.stub().returnsThis(),
       json: sinon.stub().returnsThis(),
       header: sinon.stub().returnsThis()
     }
+    adminEndpoint = admin()
   })
 
   it('should execute next if user is admin', (done) => {
     let req = {user:{admin:true}}
     let next = sinon.stub()
-    admin(req, responseSpy, next).then(() => {
+    
+    adminEndpoint(req, responseSpy, next).then(() => {
       sinon.assert.calledOnce(next)
       done()
-    }).catch(done.bind(this, 'exception caught in admin middleware'))
+    }).catch(err => done.bind(this))
   })
 
   it('should produce error if user is not admin', (done) => {
     let req = {user:{admin:false}}
     let next = sinon.stub()
-    admin(req, responseSpy, next).then(() => {
+    adminEndpoint(req, responseSpy, next).then(() => {
       sinon.assert.notCalled(next)
       let jsonCallArg = responseSpy.json.getCall(0).args[0]
       if (!jsonCallArg)
@@ -40,7 +42,7 @@ describe('admin middleware test', function() {
   it('should produce error if user admin field is missing', (done) => {
     let req = {user:{admin:false}}
     let next = sinon.stub()
-    admin(req, responseSpy, next).then(() => {
+    adminEndpoint(req, responseSpy, next).then(() => {
       sinon.assert.notCalled(next)
       let jsonCallArg = responseSpy.json.getCall(0).args[0]
       if (!jsonCallArg)
