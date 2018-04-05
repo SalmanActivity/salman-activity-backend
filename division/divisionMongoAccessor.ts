c15710d33096b05815bbd742d72cbbaeb61fdfad
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-05 10:10:01 +0700

    migrating division module to typescript

---
+++ division/divisionMongoAccessor.ts
@@ -0,0 +1,11 @@
+import { Schema, Model, Document, model } from 'mongoose'
+import DivisionAccessor from './divisionAccessor'
+import { Division } from './division'
+import { MongoAccessor } from '../accessor/mongo'
+import DivisionModel from './divisionMongoModel'
+
+export default class DivisionMongoAccessor extends MongoAccessor<Division> implements DivisionAccessor {
+  constructor() {
+    super(DivisionModel)
+  }
+}
