e115dd5b2b2808416cf5b0cf8dc3854defb66da7
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-06 06:01:05 +0700

    add get request by month in request accessor

--- request/requestAccessor.ts
+++ request/requestAccessor.ts
@@ -2,4 +2,7 @@ import Request from './request'
 import { Accessor } from '../accessor'
 
 export default interface RequestAccessor extends Accessor<Request> {
+
+  getAllBetween(start:Date, end:Date):Promise<Request[]>
+
 }
