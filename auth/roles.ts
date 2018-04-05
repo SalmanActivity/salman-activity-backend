2b0b72701dff281b9b84d831ca43f1804ed75535
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-05 10:10:48 +0700

    migrating auth module to typescript

---
+++ auth/roles.ts
@@ -0,0 +1,40 @@
+import * as util from 'util'
+import * as jwt from 'jsonwebtoken'
+import defaultConfig, { Config } from '../config'
+import { UserAccessor, UserMongoAccessor } from '../user'
+
+export async function admin(req, res, next) {
+  if (req.user && req.user.admin)
+    return next()
+  
+  return res.status(403).json({
+    error: {
+      msg: 'only admin can perform this action',
+      cause: 'unauthorized access'
+    }
+  })
+}
+
+export function user(userAccessor: UserAccessor = new UserMongoAccessor(), config:Config = defaultConfig) {
+  return async (req, res, next) => {
+    let token = undefined
+    if (req.body.token)
+      token = req.body.token
+    if (req.header('Authorization')) {
+      let tokenHeader = req.header('Authorization')
+      let tokenArr = tokenHeader.split(/\s+/)
+      if (tokenArr.length == 2 && tokenArr[0].toLowerCase() == 'jwt')
+        token = tokenArr[1]
+    }
+    req.token = token
+
+    try {
+      let verifyFunction = util.promisify(jwt.verify)
+      let data = await verifyFunction(token, config.secretKey)
+      if (data)
+        req.user = await userAccessor.getById(data.id)
+    } catch(err) {
+    }
+    next()
+  }
+}
