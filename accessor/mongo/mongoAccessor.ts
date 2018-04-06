47e2d87b6d00b139501dd55def8f3bd22c709d63
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-06 07:20:54 +0700

    fix accessor, serialize first

--- accessor/mongo/mongoAccessor.ts
+++ accessor/mongo/mongoAccessor.ts
@@ -1,20 +1,33 @@
-import Accessor from "../accessor"
-import { Model, Document, Types } from "mongoose"
-import MongoDocumentSerializer from "./mongoDocumentSerializer"
+import Accessor from '../accessor'
+import { Model, Document, Types } from 'mongoose'
+import MongoDocumentSerializer from './mongoDocumentSerializer'
 import Item from '../item'
 
 class DefaultMongoDocumentSerializer<T> implements MongoDocumentSerializer<T> {
 
   async serialize(mongoDocument: Document): Promise<T> {
-    let result:T
+    let result:any = {}
     for (let key in mongoDocument) {
-      if (result[key] instanceof Types.ObjectId)
+      if (mongoDocument[key] instanceof Types.ObjectId)
         mongoDocument.populate(key)
       if (key != '_id')
         result[key] = mongoDocument[key]
     }
     result['id'] = mongoDocument._id
     return result
+  }
+
+  async deserialize(document: T): Promise<any> {
+    let mongoDocument:any = {}
+    for (let key in document) {
+      if (key === 'id')
+        mongoDocument['_id'] = document[key]
+      else if (document[key] && typeof document[key] === 'object' && document[key]['id'])
+        mongoDocument[key] = document[key]['id']
+      else
+        mongoDocument[key] = document[key]
+    }
+    return mongoDocument
   }
 
 }
@@ -43,7 +56,8 @@ export default class MongoAccessor<T extends Item> implements Accessor<T> {
   }
 
   async insert(object: T): Promise<T> {
-    let itemModel = new this.mongoModel(object)
+    let deserializedObject = await this.docSerializer.deserialize(object)
+    let itemModel = new this.mongoModel(deserializedObject)
     if (object.id)
       itemModel._id = object.id
     let doc = await itemModel.save()
--- accessor/mongo/mongoDocumentSerializer.ts
+++ accessor/mongo/mongoDocumentSerializer.ts
@@ -4,4 +4,6 @@ export default interface MongoDocumentSerializer<T> {
 
   serialize(mongoDocument: Document): Promise<T>
 
+  deserialize(document: T): Promise<any>
+
 }
