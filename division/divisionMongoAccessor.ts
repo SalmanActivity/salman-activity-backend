bdf92dc7d07f1c8241d36ad78f91f5af83276f57
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-06 17:25:39 +0700

    update document instead of deleting document in division, and add division mongo document serializer

--- division/divisionMongoAccessor.ts
+++ division/divisionMongoAccessor.ts
@@ -1,11 +1,28 @@
 import { Schema, Model, Document, model } from 'mongoose'
 import DivisionAccessor from './divisionAccessor'
 import Division from './division'
-import { MongoAccessor } from '../accessor/mongo'
+import { MongoAccessor, MongoDocumentSerializer } from '../accessor/mongo'
 import DivisionModel from './divisionMongoModel'
+
+export class DivisionMongoDocumentSerializer implements MongoDocumentSerializer<Division> {
+  async serialize(mongoDocument: Document): Promise<Division> {
+    return {
+      id: mongoDocument._id ? mongoDocument._id.toString() : undefined,
+      name: mongoDocument.get('name'),
+      enabled: mongoDocument.get('enabled')
+    }
+  }
+  async deserialize(document: Division): Promise<any> {
+    return {
+      _id: document.id,
+      name: document.name,
+      enabled: document.enabled
+    }
+  }
+}
 
 export default class DivisionMongoAccessor extends MongoAccessor<Division> implements DivisionAccessor {
   constructor() {
-    super(DivisionModel)
+    super(DivisionModel, new DivisionMongoDocumentSerializer())
   }
 }
