7c011482ad47c834eb5ae811cb2634a21a05d23f
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-12 20:02:55 +0700

    fix location crud endpoints index

--- location/index.ts
+++ location/index.ts
@@ -6,9 +6,9 @@ export let router: Router = Router()
 
 router.get('/', auth(), crud.findAllLocations())
 router.get('/:locationId', auth(), crud.findOneLocation())
-router.post('/', auth(), admin, crud.createOneLocation())
-router.put('/:locationId', auth(), admin, crud.updateOneLocation())
-router.delete('/:locationId', auth(), admin, crud.deleteOneLocation())
+router.post('/', auth(), admin(), crud.createOneLocation())
+router.put('/:locationId', auth(), admin(), crud.updateOneLocation())
+router.delete('/:locationId', auth(), admin(), crud.deleteOneLocation())
 
 export { default as Location } from './location'
 export { default as LocationAccessor } from './locationAccessor'
