bc3e1564bfd23634da86d4202d7f53031650b712
Jauhar Arifin <jauhararifin10@gmail.com>
2018-04-18 20:02:15 +0700

    add index in photo component

---
+++ photo/index.ts
@@ -0,0 +1,8 @@
+
+export { default as Photo } from './photo'
+
+export { default as PhotoAccessor } from './photoAccessor'
+
+export { default as PhotoMongoAccessor } from './photoMongoAccessor'
+
+export { default as PhotoMongoModel } from './photoMongoModel'
